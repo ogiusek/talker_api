20ede50a683d83949718fa20367360218d8b066b
ogiusek <kowalewski.olgierd@gmail.com>
2023-07-15 17:55:27 +0200

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import { WebSocketServer } from 'ws';
 import * as http from "http";
 import { Server as io } from "socket.io";
 import express from 'express';
+import cors from "cors";
 
 import { defineSocket } from "./app";
 import defineApp from './app/defineApp';
 
 const PORT = 8080;
 const app = express();
+app.use(cors());
 const httpServer = http.createServer(app);
 const wsServer = new WebSocketServer({ server: httpServer });
 const socketServer = new io(httpServer);
