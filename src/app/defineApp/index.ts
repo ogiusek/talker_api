7e8e42b0be7c5a2695baecd8e844c8ad586b9ae3
ogiusek <kowalewski.olgierd@gmail.com>
2023-07-28 00:13:02 +0200

    Added search

--- src/app/defineApp/index.ts
+++ src/app/defineApp/index.ts
@@ -2,12 +2,14 @@ import auth from './auth/index';
 import blocked_user from './block_user/index';
 import contacts from './contacts/index';
 import messeage from './messeages/index';
+import search from "./search/index";
 
 function defineApp(app: any) {
   auth(app);
   blocked_user(app);
   messeage(app);
   contacts(app);
+  search(app);
 };
 
 export default defineApp;
