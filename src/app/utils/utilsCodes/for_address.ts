68dc61100772ad4f2853f8011803cb37d6f41e6a
ogiusek <kowalewski.olgierd@gmail.com>
2023-08-08 00:14:09 +0200

    Edited 'for_address' interface

--- src/app/utils/utilsCodes/for_address.ts
+++ src/app/utils/utilsCodes/for_address.ts
@@ -1,6 +1,6 @@
 import db from '../../db/db';
 
-function for_address(user_id: any, callback: any) {
+function for_address(user_id: any, callback: (clientAddress: string) => any) {
   db.all(`SELECT clientAddress FROM users_addresses WHERE user_id = ?;`, [user_id], (err: any, rows: any) => {
     if (err || rows.length === 0) return;
     rows.map((address: any) => {
