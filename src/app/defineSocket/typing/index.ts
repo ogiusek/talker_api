f3937977f2cd7fb7bc6300f811487fee75a07404
ogiusek <kowalewski.olgierd@gmail.com>
2023-08-10 00:07:04 +0200

    Edited socket typing

--- src/app/defineSocket/typing/index.ts
+++ src/app/defineSocket/typing/index.ts
@@ -1,7 +1,5 @@
-import Type_wait from "./type_wait/index";
 import Typing from "./type/index";
 
 export default function typing() {
-  Type_wait();
   Typing();
 } 
--- src/app/defineSocket/typing/notify/index.ts
+++ src/app/defineSocket/typing/notify/index.ts
@@ -1,21 +1,16 @@
 import db from '../../../db/db';
+import { for_address } from '../../../utils';
 import { socketEmit, clients } from '../../utils/index';
 
 function notify(user: any, to: any) {
-  db.all(`SELECT user_address FROM typing_wait WHERE user = ? AND to_user = ?`, [user, to], (err: any, waitingRows: any) => {
-    if (waitingRows.length === 0)
-      return;
+  for_address(to, (address: any) => {
+    const socket = clients[address]?.socket;
+    if (socket === undefined) return;
 
-    waitingRows.map((e: any) => {
-      const clientAddress = e.user_address;
-      if (!clients[clientAddress])
-        return;
-      const socket = clients[clientAddress].socket;
-      db.all(`SELECT user FROM typing WHERE user = ? AND to_user = ?;`, [user, to], (err: any, rows: any) => {
-        socketEmit(socket, 'typing', {
-          typer: user,
-          typing: rows.length > 0,
-        });
+    db.all(`SELECT user FROM typing WHERE user = ? AND to_user = ?;`, [user, to], (err: any, rows: any) => {
+      socketEmit(socket, 'typing', {
+        typer: user,
+        typing: rows.length > 0,
       });
     });
   });
