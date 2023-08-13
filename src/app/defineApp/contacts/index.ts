20b7b40577a39e3a02d0fc1026334e472ebf4872
ogius <kowalewski.olgierd@gmail.com>
2023-08-14 00:16:44 +0200

    Added readen info to contacts

--- src/app/defineApp/contacts/index.ts
+++ src/app/defineApp/contacts/index.ts
@@ -13,7 +13,8 @@ function contacts(app: any) {
         m.to_user || ',' || m.from_user END) AS bab,
 
           u.id, (SELECT value FROM files WHERE files.id = u.avatar_id) AS avatar, u.username, 
-          m.content_type, (SELECT value FROM files WHERE files.id = m.content_id) AS content, m.from_user
+          m.content_type, (SELECT value FROM files WHERE files.id = m.content_id) AS content, m.from_user,
+          m.readen 
         FROM messeages AS m 
         INNER JOIN users AS u ON m.from_user = u.id OR m.to_user = u.id 
         WHERE u.id != ? AND (m.from_user = ? OR m.to_user = ?) ORDER BY m.id DESC;`,
